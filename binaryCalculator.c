63fadcce924f1e1232d340122ea32d311e30012e
PratyayDhond <dhondpratyay@gmail.com>
2023-02-16 11:21:44 +0530

    Added Binary Calculator function prototypes

---
+++ binaryCalculator.c
@@ -0,0 +1,32 @@
+#include "binaryCalculator.h"
+
+int scale = 0;
+
+void setScale(int n){
+    scale = n;
+}
+
+Node * add(List l1, List l2){
+
+}
+
+Node * subtract(List l1, List l2){
+
+}
+
+Node * multiply(List l1, List l2){
+
+}
+
+Node * divide(List l1, List l2){
+
+}
+
+Node * toThePower(List l1, List l2){
+
+}
+
+Node * mod(List l1, List l2){
+
+}
+
---
+++ binaryCalculator.h
@@ -0,0 +1,9 @@
+#include "linkedList.h"
+
+void setScale(int n);
+Node * add(List l1, List l2);
+Node * subtract(List l1, List l2);
+Node * multiply(List l1, List l2);
+Node * divide(List l1, List l2);
+Node * toThePower(List l1, List l2);
+Node * mod(List l1, List l2);
